c5c7cae33ce48bf579974c8298d4f275ee90a213
TahaRizwan <tahabhaiyag@gmail.com>
2022-07-14 18:12:18 +0500

    add login Post route

--- server.js
+++ server.js
@@ -30,6 +30,27 @@ app.get('/user/:id', (req, res) => {
   })
 })
 
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  const mockUsername = 'billyTheKid'
+  const mockPassword = 'superSecret'
+
+  if (mockUsername === username && mockPassword === password) {
+    res.json({
+      success: true,
+      message: 'Password and Username match',
+      token: 'encrypted token goes here',
+    })
+  } else {
+    res.json({
+      success: false,
+      message: 'Password and Username does not match',
+    })
+  }
+})
+
 app.listen(8000, function () {
   console.log('Server running')
 })
