8b2eac1f7b5b2bdd013425bb7bfd885656146d60
TahaRizwan <tahabhaiyag@gmail.com>
2022-07-14 15:26:52 +0500

    add First get route

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 
+const mockData = [{ name: 'Mark' }, { name: 'Jill' }]
+
+const date = new Date()
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Successfully got users. Nice',
+    data: { users: mockData },
+    createdAt:
+      String(date.getDate()) +
+      ',' +
+      String(date.getMonth()) +
+      ',' +
+      String(date.getFullYear()),
+  })
+})
+
 app.listen(8000, function () {
   console.log('Server running')
 })
